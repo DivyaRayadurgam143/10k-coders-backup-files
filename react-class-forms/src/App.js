4f8ad94b4a79a9f5dab169cf13072ffff46de4df
DivyaRayadurgam143 <rayadurgamdivya379@gmail.com>
2022-12-14 17:01:43 +0530

    adding form projects

---
+++ react-class-forms/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Fform from './components/class2components';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Fform/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-class-forms/src/components/class2components.jsx
@@ -0,0 +1,141 @@
+import { Component } from "react"
+
+export default class  Fform extends Component{
+constructor(){
+    super();
+    this.state={
+        person:{
+            name:"",
+            username:"",
+            email:"",
+            password:"",
+            confirmpassword:"",
+            message:"",
+        },
+        allUsers:[
+          {
+            name:"Kim Taehyung",
+            username:"taehyung",
+            email:"v@gmail.com",
+            password:"9876543",
+            confirmpassword:"98765432",
+            message:"hii my name is v"
+          },
+          {
+            name:"Kim namjoon",
+            username:"rm",
+            email:"rm@gmail.com",
+            password:"1234567",
+            confirmpassword:"1234567",
+            message:"hii iam leader of the bts"
+          }
+        ],
+        editIndex: null,    
+   }
+}
+handleChange = (e) => {
+    var newPerson = { ...this.state.person };
+    newPerson[e.target.name] = e.target.value;
+    this.setState({ person: newPerson });
+  }
+  adduser = () => {
+    var latestUsers = [...this.state.allUsers];
+    latestUsers[this.state.editIndex] = this.state.person;
+    this.setState({ allUsers: latestUsers, editIndex: null });
+    this.clearForm();
+  }
+  clearForm = () => {
+    var newForm = {
+      name: "",
+      username: "",
+      email:"",
+      password:"",
+      confirmpassword:"",
+      message:"",
+    };
+    this.setState({ person: newForm });
+  }
+
+  editUser = (usr, i) => {
+    this.setState({ person: usr, editIndex: i });
+  }
+
+  deleteUser = (usr) => {
+    var latestUsers = this.state.allUsers.filter(
+      (myUser) => myUser.email !== usr.email
+    );
+    this.setState({ allUsers: latestUsers });
+  }
+render() {
+    return <div>
+        <form>
+          <label htmlFor="">NAME:</label>
+          <input type="text" name="name"value={this.state.person.name} onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          <label htmlFor="">USERNAME:</label>
+          <input type="text" name="username" value={this.state.person.username}onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          <label htmlFor="">EMAIL: </label>
+          <input type="text" name="email"value={this.state.person.email} onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          <label htmlFor="">PASSWORD: </label>
+          <input type="text" name="password" value={this.state.person.password}onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          <label htmlFor="">CONFIRM PASSWORD: </label>
+          <input type="text" name="confirmpassword"value={this.state.person.confirmpassword} onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          <label htmlFor="">MESSAGE: </label>
+          <textarea type="text" name="message"  cols="30" rows="10" value={this.state.person.message}onChange={(e) => {this.handleChange(e); }}/>{" "}
+          <br />
+          <br />
+          {this.state.editIndex !== null ? 
+          (
+            <button type="button" onClick={this.updateUser} className="btn btn-primary">Update User</button>
+          ) : 
+          (
+            <button type="button" onClick={this.addUser} className="btn btn-primary"> add User </button>
+          )
+          }  
+        </form>
+        <table class="table">
+  <thead>
+           <tr>
+              <th>NAME</th>
+              <th>USERNAME</th>
+              <th>EMAIL</th>
+              <th>PASSWORD</th>
+              <th>CONFIRM PASWORD</th>
+              <th>EDIT</th>
+              <th>DELETE</th>
+            </tr>
+  </thead>
+  <tbody>
+  {this.state.allUsers.map((usr, i) => (
+              <tr key={i}>
+                <td>{usr.name}</td>
+                <td>{usr.username}</td>
+                <td>{usr.email}</td>
+                <td>{usr.password}</td>
+                <td>{usr.confirmpassword}</td>
+                <td>{usr.message}</td>
+                <td>
+                  <button className="btn btn-warning" onClick={()=>{this.editUser(usr, i);}}>Edit</button>
+                </td>
+
+                <td>
+                  <button className="btn btn-danger" onClick={()=>{ this.deleteUser(usr);}}>Delete</button>{" "}
+                </td>
+
+              </tr>
+
+            ))}
+       </tbody>
+    </table>
+    </div>
+}
+}
